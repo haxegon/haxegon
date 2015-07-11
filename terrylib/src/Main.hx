190a186edaf2d7fd2c295606041b32eb50e6dcf6
Terry Cavanagh <terry@maths.tcd.ie>
2015-07-12 00:15:10 +0100

    Added support for 2D array loading into CSVs

--- terrylib/src/Main.hx
+++ terrylib/src/Main.hx
@@ -1,19 +1,6 @@
 import terrylib.*;
 
 class Main {
-	function new() {
-	  var test:Array<String> = [];
-		
-		test = Data.loadcsv_string("test");
-		
-		trace(Data.width, Data.height);
-		for (j in 0 ... Data.height) {
-			for (i in 0 ... Data.width) {
-				trace(i, j, test[i + (j * Data.width)]);
-			}
-		}
-	}
-	
 	function update() {
 		
   }
--- terrylib/src/terrylib/Data.hx
+++ terrylib/src/terrylib/Data.hx
@@ -41,6 +41,36 @@ class Data {
 		return intarray;
 	}
 	
+	public static function loadcsv_2dint(csvfile:String, delimiter:String = ","):Array<Array<Int>> {
+		tempstring = Assets.getText("data/text/" + csvfile + ".csv");
+		
+		//figure out width
+		width = 1;
+		var i:Int = 0;
+		while (i < tempstring.length) {
+			if (mid(tempstring, i) == delimiter) width++;
+			if (mid(tempstring, i) == "\n") {
+				break;
+			}
+			i++;
+		}
+		
+		tempstring = replacechar(tempstring, "\r", "");
+		tempstring = replacechar(tempstring, "\n", delimiter);
+		
+		var intarray:Array<Int> = [];
+		var stringarray:Array<String> = tempstring.split(delimiter);
+		
+		for (i in 0 ... stringarray.length) {
+			intarray.push(Std.parseInt(stringarray[i]));
+		}
+		
+		height = Std.int(intarray.length / width);
+		
+		var intarray2d:Array<Array<Int>> = [for (x in 0 ... width) [for (y in 0 ... height) intarray[x + (y * width)]]];
+		return intarray2d;
+	}
+	
 	public static function loadcsv_float(csvfile:String, delimiter:String = ","):Array<Float> {
 		tempstring = Assets.getText("data/text/" + csvfile + ".csv");
 		
@@ -69,6 +99,36 @@ class Data {
 		return floatarray;
 	}
 	
+	public static function loadcsv_2dfloat(csvfile:String, delimiter:String = ","):Array<Array<Float>> {
+		tempstring = Assets.getText("data/text/" + csvfile + ".csv");
+		
+		//figure out width
+		width = 1;
+		var i:Int = 0;
+		while (i < tempstring.length) {
+			if (mid(tempstring, i) == delimiter) width++;
+			if (mid(tempstring, i) == "\n") {
+				break;
+			}
+			i++;
+		}
+		
+		tempstring = replacechar(tempstring, "\r", "");
+		tempstring = replacechar(tempstring, "\n", delimiter);
+		
+		var floatarray:Array<Float> = [];
+		var stringarray:Array<String> = tempstring.split(delimiter);
+		
+		for (i in 0 ... stringarray.length) {
+			floatarray.push(Std.parseFloat(stringarray[i]));
+		}
+		
+		height = Std.int(floatarray.length / width);
+		
+		var floatarray2d:Array<Array<Float>> = [for (x in 0 ... width) [for (y in 0 ... height) floatarray[x + (y * width)]]];
+		return floatarray2d;
+	}
+	
 	public static function loadcsv_string(csvfile:String, delimiter:String = ","):Array<String> {
 		tempstring = Assets.getText("data/text/" + csvfile + ".csv");
 		
@@ -90,6 +150,31 @@ class Data {
 		height = Std.int(stringarray.length / width);
 		return stringarray;
 	}
+	
+	public static function loadcsv_2dstring(csvfile:String, delimiter:String = ","):Array<Array<String>> {
+		tempstring = Assets.getText("data/text/" + csvfile + ".csv");
+		
+		//figure out width
+		width = 1;
+		var i:Int = 0;
+		while (i < tempstring.length) {
+			if (mid(tempstring, i) == delimiter) width++;
+			if (mid(tempstring, i) == "\n") {
+				break;
+			}
+			i++;
+		}
+		
+		tempstring = replacechar(tempstring, "\r", "");
+		tempstring = replacechar(tempstring, "\n", delimiter);
+		
+		var stringarray:Array<String> = tempstring.split(delimiter);
+		height = Std.int(stringarray.length / width);
+		
+		var stringarray2d:Array<Array<String>> = [for (x in 0 ... width) [for (y in 0 ... height) stringarray[x + (y * width)]]];
+		return stringarray2d;
+	}
+	
 	
 	/** Return characters from the middle of a string. */
 	private static function mid(currentstring:String, start:Int = 0, length:Int = 1):String {
