640fdae090aaa6a0d5e4306969874486ee5140c8
Terry Cavanagh <terry@maths.tcd.ie>
2016-01-04 16:10:09 +0000

    Working on wordwrap, renamed Text.len to Text.width, fixes #47

--- haxegon/src/Main.hx
+++ haxegon/src/Main.hx
@@ -4,19 +4,26 @@ class Main {
 	function new() {
 	}
 
-	function inbox(x, y, boxx, boxy, boxw, boxh){
-		if(x >= boxx && x < boxx + boxw){
-			if(y >= boxy && y < boxy + boxh){
-				return true;
-			}
-		}
-		return false;
-	}
-
+	var wrappedtext:Array<String>;
+	
+	var textwidth:Int = 400;
+	
 	function update(){
-		Gfx.drawbox(50, 50, 25, 25, Col.WHITE);
-		if(inbox(Mouse.x, Mouse.y, 50, 50, 25, 25)){
-			Gfx.fillbox(50, 50, 25, 25, Col.RED);
+		Text.changesize(16);
+		Text.display(0, 0, "Word wrap example!");
+		
+		Text.changesize(12);
+		wrappedtext = Text.wordwrap(textwidth, "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+		
+		Gfx.fillbox(18, 38, textwidth + 4, 4 + (wrappedtext.length * 12), Col.DARKBLUE);
+		for (i in 0 ... wrappedtext.length) {
+		  Text.display(20, 40 + (i * 12) - 4, wrappedtext[i]);	
+		}
+		
+		if (Input.pressed(Key.LEFT)) {
+		  textwidth -= 5;	
+		}else if (Input.pressed(Key.RIGHT)) {
+		  textwidth += 5;	
 		}
 	}
 }
