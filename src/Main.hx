0c898e82801d84d886ee4626d8e3f69a702444f7
TerryCavanagh <terrygcavanagh@gmail.com>
2016-11-13 18:28:38 +0000

    Fixed default bitmap font, simple test app

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,17 @@
 import haxegon.*;
 
 class Main {
-	function new() {
-	}
-	
+	var gametime:Float = 0;
 	function update() {
-		Text.display(Text.CENTER, Text.BOTTOM - 2, "Haxegon test");
+		gametime+= 1;
+		
+		for (i in 0 ... 24) {
+		  Gfx.fillbox(i * 10, i * 10, Gfx.screenwidth - (i * 20), Gfx.screenheight - (i * 20), 
+			            Gfx.hsl((gametime + i * 15) % 360, 0.5, 0.5));
+		}
+		
+		Text.setfont("default", 2);
+		Text.display(Text.CENTER + 1, Text.CENTER + 1, "Haxegon testing", Col.BLACK);
+		Text.display(Text.CENTER, Text.CENTER, "Haxegon testing");
 	}
 }
