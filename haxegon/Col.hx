7743bb602f6134077e2389cd62f28a66ea62a048
TerryCavanagh <terrygcavanagh@gmail.com>
2016-12-07 11:05:00 +0000

    Moved colour functions from Gfx.hx to Col.hx. Fixes #102

--- haxegon/Col.hx
+++ haxegon/Col.hx
@@ -23,4 +23,112 @@ class Col {
 	public static var MAGENTA:Int = 0xFF00FF;
 	
 	public static var TRANSPARENT:Int = 0x000001;
+	
+	public static inline function getred(c:Int):Int {
+		return ((c >> 16) & 0xFF);
+	}
+	
+	public static inline function getgreen(c:Int):Int {
+		return ((c >> 8) & 0xFF);
+	}
+	
+	public static inline function getblue(c:Int):Int {
+		return (c & 0xFF);
+	}
+	
+	/** Get the Hue value (0-360) of a hex code colour. **/
+	public static function gethue(c:Int):Int {	
+    var r:Float = getred(c) / 255;
+		var g:Float = getgreen(c) / 255;
+		var b:Float = getblue(c) / 255;
+    var max:Float = Math.max(Math.max(r, g), b); 
+		var min:Float = Math.min(Math.min(r, g), b); 
+    
+		var h:Float = (max + min) / 2;
+		
+    if (max != min) {
+			var d:Float = max - min;
+			if(max == r){
+				h = (g - b) / d + (g < b ? 6 : 0);
+			}else if (max == g) {
+				h = (b - r) / d + 2;
+			}else if (max == b) {
+				h = (r - g) / d + 4;
+			}
+			h /= 6;
+    }
+		
+    return Std.int(h * 360);
+	}
+	
+	/** Get the Saturation value (0.0-1.0) of a hex code colour. **/
+	public static function getsaturation(c:Int):Float {
+    var r:Float = getred(c) / 255;
+		var g:Float = getgreen(c) / 255;
+		var b:Float = getblue(c) / 255;
+    var max:Float = Math.max(Math.max(r, g), b); 
+		var min:Float = Math.min(Math.min(r, g), b); 
+    
+		var s:Float = (max + min) / 2;
+		var l:Float = s;
+		
+    if (max == min) {
+			s = 0;
+    }else {
+			var d:Float = max - min;
+			s = l > 0.5?d / (2 - max - min):d / (max + min);
+    }
+		
+    return s;
+	}
+	
+	/** Get the Lightness value (0.0-1.0) of a hex code colour. **/
+	public static function getlightness(c:Int):Float {
+		var r:Float = getred(c) / 255;
+		var g:Float = getgreen(c) / 255;
+		var b:Float = getblue(c) / 255;
+    var max:Float = Math.max(Math.max(r, g), b); 
+		var min:Float = Math.min(Math.min(r, g), b); 
+		
+    return (max + min) / 2;
+	}
+	
+	public static function rgb(red:Int, green:Int, blue:Int):Int {
+		return (blue | (green << 8) | (red << 16));
+	}
+	
+	/** Picks a colour given Hue, Saturation and Lightness values. 
+	 *  Hue is between 0-359, Saturation and Lightness between 0.0 and 1.0. */
+	public static function hsl(hue:Float, saturation:Float, lightness:Float):Int {
+		var q:Float = if (lightness < 1 / 2) {
+			lightness * (1 + saturation);
+		}else {
+			lightness + saturation - (lightness * saturation);
+		}
+		
+		var p:Float = 2 * lightness - q;
+		
+		var hk:Float = ((hue % 360) / 360);
+		
+		hslval[0] = hk + 1 / 3;
+		hslval[1] = hk;
+		hslval[2] = hk - 1 / 3;
+		for (n in 0 ... 3){
+			if (hslval[n] < 0) hslval[n] += 1;
+			if (hslval[n] > 1) hslval[n] -= 1;
+			hslval[n] = if (hslval[n] < 1 / 6){
+				p + ((q - p) * 6 * hslval[n]);
+			}else if (hslval[n] < 1 / 2)	{
+				q;
+			}else if (hslval[n] < 2 / 3){
+				p + ((q - p) * 6 * (2 / 3 - hslval[n]));
+			}else{
+				p;
+			}
+		}
+		
+		return rgb(Std.int(hslval[0] * 255), Std.int(hslval[1] * 255), Std.int(hslval[2] * 255));
+	}
+	//HSL conversion variables 
+	private static var hslval:Array<Float> = [0.0, 0.0, 0.0];
 }
