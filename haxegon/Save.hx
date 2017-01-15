f86dca68b7f388654245f16368a9d0beeb40cad1
TerryCavanagh <terrygcavanagh@gmail.com>
2017-01-15 16:26:09 +0000

    Added some new stuff to Save.hx (Save.keys, Save.fileexists()), fixes #120, fixes #114

--- haxegon/Save.hx
+++ haxegon/Save.hx
@@ -4,25 +4,6 @@ import openfl.net.SharedObject;
 import openfl.net.SharedObjectFlushStatus;
 
 class Save {
-  public static var filename(get, set):String;
-	private static var _filename:String = "";
-	
-	static function get_filename():String {
-		return _filename;
-	}
-	
-	static function set_filename(newsavefile:String):String {
-	  if(filename != newsavefile){
-      changesavefile(newsavefile);
-    }
-		return _filename;
-	}
-	
-	private static function changesavefile(name:String) {
-		_filename = name;
-		so = SharedObject.getLocal(_filename);
-	}
-	
 	public static function save(key:String, value:Dynamic) {
 	  if (so == null) changesavefile("haxegongame");
 		
@@ -33,11 +14,20 @@ class Save {
 		} catch (e:Dynamic) {
 		  Debug.log("Error: Unable to save \"" + key + "\"."); 
 		}
+		
+		keylistdirty = true;
 	}
 	
 	public static function exists(key:String):Bool {
 	  if (so == null) changesavefile("haxegongame");
 		if (Reflect.field(so.data, key) == null) return false;
+		return true;
+	}
+		
+	public static function fileexists(savefile:String):Bool {
+		var tempso:SharedObject = SharedObject.getLocal(savefile);
+		
+		if (Reflect.fields(tempso.data).length == 0) return false;
 		return true;
 	}
 	
@@ -54,13 +44,54 @@ class Save {
 	
 	public static function delete(?name:String) {
 		if (name == null) name = "haxegongame";
-		if (_savefile != name) {
+		if (_filename != name) {
 			var newso:SharedObject = SharedObject.getLocal(name);
 			newso.clear();
 		}else {
 		  so.clear();	
 		}
+		
+		keylistdirty = true;
+	}
+	
+	public static var keys(get, never):Array<String>;
+	
+	static function get_keys():Array<String> {
+	  if (so == null) changesavefile("haxegongame");
+		
+		if (keylistdirty) {
+			var newkeylist:Array<String> = Reflect.fields(so.data);
+			keylist = [];
+			for(i in 0 ... newkeylist.length){
+				keylist.push(newkeylist[i]);
+			}
+			keylistdirty = false;
+		}
+		
+		return keylist;
+	}
+	
+	private static function changesavefile(name:String) {
+		_filename = name;
+		so = SharedObject.getLocal(_filename);
+		keylistdirty = true;
+	}
+	
+  public static var filename(get, set):String;
+	private static var _filename:String = "";
+	
+	static function get_filename():String {
+		return _filename;
+	}
+	
+	static function set_filename(newsavefile:String):String {
+	  if(filename != newsavefile){
+      changesavefile(newsavefile);
+    }
+		return _filename;
 	}
 	
 	private static var so:SharedObject;	
+	private static var keylist:Array<String>;
+	private static var keylistdirty:Bool = true;
 }
