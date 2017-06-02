dd02cb279d62475eea90c204a3d4c385fee35810
TerryCavanagh <terrygcavanagh@gmail.com>
2017-06-02 16:30:56 +0100

    Removed some old Random.hx clutter functions

--- haxegon/Random.hx
+++ haxegon/Random.hx
@@ -6,19 +6,9 @@ class Random{
 		return random() < 0.5;
 	}
 	
-	/** True 1/5th of the time */
-	public static function occasional():Bool{
-		return random() < 0.2;
-	}
-
-	/** True 5% or 1/20th of the time */
-	public static function rare():Bool{
-		return random() < 0.05;
-	}
-	
-	/** Returns true n% of the time, where n is a number between 0-100, inclusive. */
-	public static function chance(n:Int):Bool{
-		return int(0, 100) <= n;
+	/** Returns true n% of the time, where n is a float between 0-100, inclusive. */
+	public static function chance(n:Float):Bool{
+		return float(0, 100) <= n;
 	}
 	
 	/** Return a random integer between 'from' and 'to', inclusive. */
@@ -52,108 +42,6 @@ class Random{
 		return str;
 	}
 
-	/** Return a random string from a list of up to 12 strings. */
-	public static function pickstring(s1:String, s2:String, ?s3:String, ?s4:String,
-																					 ?s5:String, ?s6:String, ?s7:String, ?s8:String,
-																					 ?s9:String, ?s10:String, ?s11:String, ?s12:String):String{
-	  temp = 2;
-		if (s3 != null) temp = 3;
-	  if (s4 != null) temp = 4;
-	  if (s5 != null) temp = 5;
-	  if (s6 != null) temp = 6;
-	  if (s7 != null) temp = 7;
-	  if (s8 != null) temp = 8;
-	  if (s9 != null) temp = 9;
-	  if (s10 != null) temp = 10;
-	  if (s11 != null) temp = 11;
-	  if (s12 != null) temp = 12;
-		
-		switch(int(1, temp)) {
-			case 1: return s1;
-			case 2: return s2;
-			case 3: return s3;
-			case 4: return s4;
-			case 5: return s5;
-			case 6: return s6;
-			case 7: return s7;
-			case 8: return s8;
-			case 9: return s9;
-			case 10: return s10;
-			case 11: return s11;
-			case 12: return s12;
-		}
-		
-		return s1;
-	}
-	
-	/** Return a random Int from a list of up to 12 Ints. */
-	public static function pickint(s1:Int, s2:Int, s3:Int = -10000, s4:Int = -10000,
-																					 s5:Int = -10000, s6:Int = -10000, s7:Int = -10000, s8:Int = -10000,
-																					 s9:Int = -10000, s10:Int = -10000, s11:Int = -10000,s12:Int = -10000):Int{
-	  temp = 2;
-    if (s3 != -10000) temp = 3;
-	  if (s4 != -10000) temp = 4;
-	  if (s5 != -10000) temp = 5;
-	  if (s6 != -10000) temp = 6;
-	  if (s7 != -10000) temp = 7;
-	  if (s8 != -10000) temp = 8;
-	  if (s9 != -10000) temp = 9;
-	  if (s10 != -10000) temp = 10;
-	  if (s11 != -10000) temp = 11;
-	  if (s12 != -10000) temp = 12;
-		
-		switch(int(1, temp)) {
-			case 1: return s1;
-			case 2: return s2;
-			case 3: return s3;
-			case 4: return s4;
-			case 5: return s5;
-			case 6: return s6;
-			case 7: return s7;
-			case 8: return s8;
-			case 9: return s9;
-			case 10: return s10;
-			case 11: return s11;
-			case 12: return s12;
-		}
-		
-		return s1;
-	}
-	
-	/** Return a random Float from a list of up to 12 Floats. */
-	public static function pickfloat(s1:Float, s2:Float, s3:Float = -10000, s4:Float = -10000,
-																					 s5:Float = -10000, s6:Float = -10000, s7:Float = -10000, s8:Float = -10000,
-																					 s9:Float = -10000, s10:Float = -10000, s11:Float = -10000,s12:Float = -10000):Float{
-	  temp = 2;
-    if (s3 != -10000) temp = 3;
-	  if (s4 != -10000) temp = 4;
-	  if (s5 != -10000) temp = 5;
-	  if (s6 != -10000) temp = 6;
-	  if (s7 != -10000) temp = 7;
-	  if (s8 != -10000) temp = 8;
-	  if (s9 != -10000) temp = 9;
-	  if (s10 != -10000) temp = 10;
-	  if (s11 != -10000) temp = 11;
-	  if (s12 != -10000) temp = 12;
-		
-		switch(int(1, temp)) {
-			case 1: return s1;
-			case 2: return s2;
-			case 3: return s3;
-			case 4: return s4;
-			case 5: return s5;
-			case 6: return s6;
-			case 7: return s7;
-			case 8: return s8;
-			case 9: return s9;
-			case 10: return s10;
-			case 11: return s11;
-			case 12: return s12;
-		}
-		
-		return s1;
-	}
-	
 	public static function pick<T>(arr:Array<T>):T {
 		return arr[int(0, arr.length - 1)];
 	}
