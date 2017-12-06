b2df6cf909a222782f99fbcfdef77a2d49fc5a79
TerryCavanagh <terrygcavanagh@gmail.com>
2017-12-06 20:54:45 +0000

    Removed linker statements (has been added to lime 5.9.2)

--- haxegon/Load.hx
+++ haxegon/Load.hx
@@ -4,15 +4,6 @@ import openfl.display.*;
 import openfl.events.Event;
 import starling.core.Starling;
 
-@:cppFileCode('
-#if defined(HX_WINDOWS)
-   extern "C" {
-      _declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
-      _declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
-   }
-   #endif
-')
-
 @:access(haxegon.Core)
 @:keep class Load extends Sprite{
 	var starling:Starling;
